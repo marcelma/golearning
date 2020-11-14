d4f58c785809a027aac18d4e1081d393f7e051bc
Marcell Martini <marcellmartini@gmail.com>
2020-11-13 20:28:48 -0400

    feat: Add slice example

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 func main() {
 	arrayExample()
+	sliceExample()
 }
 
 func headerFunc(h string) {
@@ -21,4 +22,20 @@ func arrayExample() {
 	a := []int{1, 2, 3, 4}
 
 	fmt.Printf("%v\t %T\n", a, a)
+}
+
+func sliceExample() {
+	headerFunc("sliceExample")
+
+	x := []int{4, 5, 7, 8, 42}
+	fmt.Println(x)
+	x = append(x, 77, 88, 99, 1014)
+	fmt.Println(x)
+
+	y := []int{234, 456, 678, 987}
+	x = append(x, y...)
+	fmt.Println(x)
+
+	z := append(x[0:2], x[4:]...)
+	fmt.Println(z)
 }
